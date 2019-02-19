7a3d5ae4725dda274747fd68c7d095bd9166e028
Meet Shah <meetdshah007@gmail.com>
2019-02-19 13:07:08 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,13 +3,13 @@ var index = require("../index.js")
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Joe"', () => {
-      expect(index.name).toEqual('Joe')
+      expect(index.name).toEqual('Susan')
     })
   })
 
 
   describe('Height', () => {
-    it('is less than 74', () => {
+    it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
     })
   })
